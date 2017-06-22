9d244a52103f6e6d1aeb1df83fb734c1b13ca62a
bclemenzi <brendan@clemenzi.com>
2017-06-22 16:45:24 -0400

    Updates to support emailing file attachments.

--- src/main/java/com/nfbsoftware/sansserverplugin/sdk/aws/AmazonSESManager.java
+++ src/main/java/com/nfbsoftware/sansserverplugin/sdk/aws/AmazonSESManager.java
@@ -1,11 +1,21 @@
 package com.nfbsoftware.sansserverplugin.sdk.aws;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
@@ -16,7 +26,9 @@ import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.RawMessage;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.simpleemail.model.SendRawEmailRequest;
 import com.nfbsoftware.sansserverplugin.sdk.util.Entity;
 import com.nfbsoftware.sansserverplugin.sdk.util.StringUtil;
 
@@ -54,7 +66,6 @@ public class AmazonSESManager
         m_amazonSimpleEmailServiceClient.setRegion(Region.getRegion(Regions.fromName(regionName)));
     }
     
-
     /**
      * 
      * @param toEmail
@@ -64,10 +75,59 @@ public class AmazonSESManager
      */
     public void buildEmailMessage(String toEmail, String messageSubject, String messageBody) throws Exception
     {
+        String replyEmailAddress = StringUtil.emptyIfNull(m_properties.getProperty(Entity.FrameworkProperties.AWS_SES_REPLY_EMAIL_ADDRESS));
+        
         List<String> toAddresses = new ArrayList<String>();
         toAddresses.add(toEmail);
         
-        buildEmailMessage(toAddresses, messageSubject, messageBody);
+        buildEmailMessage(toAddresses, replyEmailAddress, messageSubject, messageBody, null);
+    }
+    
+    /**
+     * 
+     * @param toEmail
+     * @param messageSubject
+     * @param messageBody
+     * @throws Exception
+     */
+    public void buildEmailMessage(String toEmail, String messageSubject, String messageBody, File fileAttachment) throws Exception
+    {
+        String replyEmailAddress = StringUtil.emptyIfNull(m_properties.getProperty(Entity.FrameworkProperties.AWS_SES_REPLY_EMAIL_ADDRESS));
+        
+        List<String> toAddresses = new ArrayList<String>();
+        toAddresses.add(toEmail);
+        
+        buildEmailMessage(toAddresses, replyEmailAddress, messageSubject, messageBody, fileAttachment);
+    }
+    
+    /**
+     * 
+     * @param toEmail
+     * @param messageSubject
+     * @param messageBody
+     * @throws Exception
+     */
+    public void buildEmailMessage(String toEmail, String replyEmailAddress, String messageSubject, String messageBody) throws Exception
+    {
+        List<String> toAddresses = new ArrayList<String>();
+        toAddresses.add(toEmail);
+        
+        buildEmailMessage(toAddresses, replyEmailAddress, messageSubject, messageBody, null);
+    }
+    
+    /**
+     * 
+     * @param toEmail
+     * @param messageSubject
+     * @param messageBody
+     * @throws Exception
+     */
+    public void buildEmailMessage(String toEmail, String replyEmailAddress, String messageSubject, String messageBody, File fileAttachment) throws Exception
+    {
+        List<String> toAddresses = new ArrayList<String>();
+        toAddresses.add(toEmail);
+        
+        buildEmailMessage(toAddresses, replyEmailAddress, messageSubject, messageBody, fileAttachment);
     }
     
     /**
@@ -77,27 +137,66 @@ public class AmazonSESManager
      * @param messageBody
      * @throws Exception
      */
-    public void buildEmailMessage(List<String> toAddresses, String messageSubject, String messageBody) throws Exception
+    public void buildEmailMessage(List<String> toAddresses, String replyEmailAddress, String messageSubject, String messageBody, File fileAttachment) throws Exception
     {
-        String replyEmailAddress = StringUtil.emptyIfNull(m_properties.getProperty(Entity.FrameworkProperties.AWS_SES_REPLY_EMAIL_ADDRESS));
+        // If we don't have an attachment, send a simple email
+        if(fileAttachment == null)
+        {
+            SendEmailRequest emailRequest = new SendEmailRequest();
+            emailRequest.withSource(replyEmailAddress);
         
-        SendEmailRequest emailRequest = new SendEmailRequest();
-        emailRequest.withSource(replyEmailAddress);
+            Destination dest = new Destination().withToAddresses(toAddresses);
+            emailRequest.setDestination(dest);
     
-        Destination dest = new Destination().withToAddresses(toAddresses);
-        emailRequest.setDestination(dest);
-
-        Content subjContent = new Content().withData(messageSubject);
-        Message msg = new Message().withSubject(subjContent);
+            Content subjContent = new Content().withData(messageSubject);
+            Message msg = new Message().withSubject(subjContent);
+        
+            // Include a body in both text and HTML formats
+            Content htmlContent = new Content().withData(messageBody);
+            
+            Body body = new Body().withHtml(htmlContent);
+            msg.setBody(body);
     
-        // Include a body in both text and HTML formats
-        Content htmlContent = new Content().withData(messageBody);
-        
-        Body body = new Body().withHtml(htmlContent);
-        msg.setBody(body);
+            emailRequest.setMessage(msg);
+            
+            m_amazonSimpleEmailServiceClient.sendEmail(emailRequest);
+        }
+        else
+        {
+            // Since we have a file attachment, send a raw email; which is a bit more complicated
+            Session session = Session.getInstance(new Properties()); 
+            session.setDebug(false);
 
-        emailRequest.setMessage(msg);
-        
-        m_amazonSimpleEmailServiceClient.sendEmail(emailRequest);
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(replyEmailAddress));
+
+            message.setSubject(messageSubject);
+            
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+            messageBodyPart.setContent(messageBody, "text/html");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(messageBodyPart);
+
+            if (null != fileAttachment) 
+            {
+                MimeBodyPart attachmentPart = new MimeBodyPart();
+                attachmentPart.attachFile(fileAttachment);
+                multipart.addBodyPart(attachmentPart);
+            }
+
+            message.setContent(multipart);
+            
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            message.writeTo(outputStream);
+            
+            RawMessage rawMessage = new RawMessage(ByteBuffer.wrap(outputStream.toByteArray()));
+            
+            SendRawEmailRequest rawEmailRequest = new SendRawEmailRequest(rawMessage);
+            rawEmailRequest.setDestinations(toAddresses);
+            rawEmailRequest.setSource(replyEmailAddress);
+            
+            m_amazonSimpleEmailServiceClient.sendRawEmail(rawEmailRequest);
+        }
     }
 }
